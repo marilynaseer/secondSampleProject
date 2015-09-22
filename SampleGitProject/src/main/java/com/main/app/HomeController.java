fbe6d8ff0673dda7499935e59855c09342bff404
merl <merl@merl-PC>
2015-09-22 15:40:58 +0530

    Second commit

--- SampleGitProject/src/main/java/com/main/app/HomeController.java
+++ SampleGitProject/src/main/java/com/main/app/HomeController.java
@@ -28,7 +28,7 @@ public class HomeController {
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+		System.out.println("Adding changes");
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
